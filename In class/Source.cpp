a15a9da2fe2ff63344116608ca018d1772579333
Matthew Brown <MBrown279@my.gcu.edu>
2020-01-10 14:51:41 -0700

    Added 3 in class codes

---
+++ In class/Source.cpp
@@ -0,0 +1,22 @@
+//Matthew Brown
+//This is my own work
+//In class excercise 1/8/2020
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	float kilogram, pound;
+	cout << "Enter pound";
+	cin >> pound;
+	kilogram = pound * .454;
+	cout << pound << "pound=" << kilogram << "kg";
+		return 0;
+}
+
+
+
+
+
+
